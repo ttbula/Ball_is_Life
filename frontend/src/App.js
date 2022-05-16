8c5dd3195b7efd30cd3c8b2083b1592c1e10a548
mo4rahman <mo4rahman@gmail.com>
2022-05-16 11:00:55 -0400

    create router structure and route paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Player from "./pages/Player";
+import Rosters from "./pages/Rosters";
 
 function App() {
-  return <div>Ya Yeet</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/player" element={<Player />} />
+        <Route path="/rosters" element={<Rosters />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>Home Page of all Teams</div>;
+}
+
+export default Home;
